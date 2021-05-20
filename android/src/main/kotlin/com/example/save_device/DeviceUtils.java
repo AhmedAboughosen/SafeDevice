f0e574fc0eb6307a200d50ac62ad0990e6849a74
Ahmed Aboughosen <ahmed.a_naser@outlook.com>
2021-05-20 12:15:43 +0200

    print path
    Signed-off-by: Ahmed Aboughosen <55014628+AhmedAboughosen@users.noreply.github.com>

--- android/src/main/kotlin/com/example/save_device/DeviceUtils.java
+++ android/src/main/kotlin/com/example/save_device/DeviceUtils.java
@@ -36,6 +36,7 @@ public class DeviceUtils {
                 "L3N5c3RlbS9iaW4vZmFpbHNhZmUvc3U=",
                 "L2RhdGEvbG9jYWwvc3U=", "L3N1L2Jpbi9zdQ=="};
         for (String path : paths) {
+            System.out.println(path);
             if (new File(decodeString(path)).exists()) return true;
         }
         return false;
